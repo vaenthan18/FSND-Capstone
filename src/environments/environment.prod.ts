e266f4d5132b3b847ed1a082821e8b5cc16784e8
Vaenthan Jeevarajah <vaenthan.j@gmail.com>
2020-05-28 18:14:53 -0400

    HEROKU DEPLOYMENT

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  auth0: {
+    url: 'fsnd-vaenthan', // the auth0 domain prefix
+    audience: 'casting', // the audience set for the auth0 app
+    clientId: 'KZpq6rVEBOioAXspVX3z7919j1YxpVUN', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+  }
 };
