fb0713e29dfbd65d2f6b8e9bc9efbdc87af68b17
Vaenthan Jeevarajah <vaenthan.j@gmail.com>
2020-05-29 00:32:47 -0400

    HEROKU DEPLOYMENT

--- backend/manage/manage.py
+++ backend/manage/manage.py
@@ -1,8 +1,8 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from ..app import app
-from ..database.models import db
+from app.app import app
+from database.models import db
 
 migrate = Migrate(app, db)
 manager = Manager(app)
